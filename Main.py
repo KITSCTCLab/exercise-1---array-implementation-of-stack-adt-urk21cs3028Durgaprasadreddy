5818d36cde20f4c22e83dbc8c0b6355cc45fadd5
urk21cs3028Durgaprasadreddy <109951970+urk21cs3028Durgaprasadreddy@users.noreply.github.com>
2022-07-25 16:31:15 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,26 +1,26 @@
 import os
 class Stack:
     def __init__(self, size):
-        self.items = []
-        self.size = size
+        self.items = [NONE]*size
+        self.top = -1
 
     def is_empty(self):
-        # Write code here
+        return self.top== -1
+     
 
     def is_full(self):
-        # Write code here
-
+        return self.top==(self.size-1)
     def push(self, data):
         if not self.is_full():
-            # Write code here
+            self.top=+1
+            self.items[self.top]=data
 
     def pop(self):
         if not self.is_empty():
-            # Write code here
-
+            x=self.item[self.top]
     def status(self):
-        # Write code here
-
+        for i in range(self.top=1):
+            print(self.item[i])
 # Do not change the following code
 size, queries = map(int, input().rstrip().split())
 stack = Stack(size)
